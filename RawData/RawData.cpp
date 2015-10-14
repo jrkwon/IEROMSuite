d2f5a3562796d0e5cbf975021b272ef21db3c244
Jaerock Kwon <jaerock.kwon@gmail.com>
2015-10-14 16:00:49 -0400

    Tested RawData class with main.cpp.

--- RawData/RawData.cpp
+++ RawData/RawData.cpp
@@ -1,7 +1,78 @@
+#include <QDir>
+#include <QJsonDocument>
+
 #include "RawData.h"
 
-RawData::RawData()
-{
+IEROM_NAMESPACE_START
 
+void TissueSample::read(const QJsonObject &json)
+{
+    this->id = json["ID"].toString();
+    this->specimen = json["Specimen"].toString();
+    this->organ = json["Orgain"].toString();
+    this->stain = json["Stain"].toString();
+    this->object = json["Object"].toString();
+    this->sectioningPlane = json["Sectioning Plane"].toString();
+    this->scope = json["Scope"].toString();
+    QJsonObject acqDateJsonObj = json["Acquisition Date"].toObject();
+    this->startEndDate.start = acqDateJsonObj["Start"].toString();
+    this->startEndDate.end = acqDateJsonObj["End"].toString();
+    QJsonObject sizeJsonObject = json["Size"].toObject();
+    this->size.width = sizeJsonObject["Width"].toInt();
+    this->size.depth = sizeJsonObject["Depth"].toInt();
+    this->size.height = sizeJsonObject["Height"].toInt();
 }
 
+void Sectioning::read(const QJsonObject &json)
+{
+    this->knifeEdge = json["Knife Edge"].toString();
+    QJsonObject vrJsonObj = json["Voxel Resol"].toObject();
+    this->voxelResol.width = vrJsonObj["Width"].toDouble();
+    this->voxelResol.depth = vrJsonObj["Depth"].toDouble();
+    this->voxelResol.height = vrJsonObj["Height"].toDouble();
+    QJsonObject ssJsonObj = json["Slice Size"].toObject();
+    this->sliceSize.width = ssJsonObj["Width"].toInt();
+    this->sliceSize.height = ssJsonObj["Height"].toInt();
+    QJsonObject coJsonObj = json["Columns"].toObject();
+    this->columns.start = coJsonObj["Start"].toInt();
+    this->columns.end = coJsonObj["End"].toInt();
+    QJsonObject rzJsonObj = json["RangeZ"].toObject();
+    this->rangeZ.minZ = rzJsonObj["MinZ"].toDouble();
+    this->rangeZ.maxZ = rzJsonObj["MaxZ"].toDouble();
+}
+
+RawData::RawData(QString basePath, QString rawDataJsonFileName)
+{
+    this->basePath = basePath;
+    this->rawDataJsonFileName = rawDataJsonFileName;
+}
+
+bool RawData::loadRawData()
+{
+    QString jsonFilePath = QDir::cleanPath(this->basePath + "/" + this->rawDataJsonFileName);
+    //qDebug() << jsonFilePath;
+
+    QFile settingsFile(jsonFilePath);
+    if(!settingsFile.open(QIODevice::ReadOnly)) {
+        qWarning("Couldn't open the json file.");
+        return false;
+    }
+    QByteArray settingsByteArray = settingsFile.readAll();
+    QJsonDocument jsonDoc = QJsonDocument::fromJson(settingsByteArray);
+    QJsonObject json = jsonDoc.object();
+
+    read(json);
+
+    return true;
+}
+
+void RawData::read(const QJsonObject &json)
+{
+    QJsonObject tsJson = json["Tissue Sample"].toObject();
+    this->tissueSample.read(tsJson);
+
+    QJsonObject secJson = json["Sectioning"].toObject();
+    this->sectioning.read(secJson);
+}
+
+IEROM_NAMESPACE_END
--- RawData/RawData.pro
+++ RawData/RawData.pro
@@ -15,4 +15,12 @@ CONFIG   -= app_bundle
 TEMPLATE = app
 
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    RawData.cpp \
+    ../Settings/Settings.cpp
+
+HEADERS += \
+    RawData.h \
+    ../Settings/Settings.h
+
+INCLUDEPATH += $$PWD/../Settings
